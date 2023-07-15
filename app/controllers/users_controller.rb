8fda1abde34ab738086e30bd333f1e9bc3516526
crisandryj <crisandryj@gmail.com>
2023-07-15 13:51:27 -0400

    Feat: Users create and new actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end 
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to @user
+    else
+       render :new, status: :unprocessable_entity
+  end 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:user_id)
+  end
+end
