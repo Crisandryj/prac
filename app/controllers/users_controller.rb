98d10c5697b610766f6a01c977c21bd70292b82c
crisandryj <crisandryj@gmail.com>
2023-07-15 13:56:38 -0400

    user resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,:email,:user_id)
+    params.require(:user).permit(:name,:email,:id)
   end
 end
